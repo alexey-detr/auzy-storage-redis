72dd7de963fa78fdc0701b0de7dfd32a2cc1df28
Alexey Ponomarev <xlz@ya.ru>
2017-12-10 14:32:50 +0700

    refactor: Refactored TTL usage to support new version of auzy
    BREAKING CHANGE: TTL has to be specified in milliseconds now

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ module.exports = class RedisStorage {
     set(key, value, ttl = null) {
         let promise = this.redis.set(key, JSON.stringify(value));
         if (ttl) {
-            promise = this.redis.expire(key, ttl);
+            promise = this.redis.expire(key, Math.round(ttl / 1000));
         }
         return promise;
     }
 
     get(key) {
-        return this.redis.get(key).then(value => Promise.resolve(JSON.parse(value)));
+        return this.redis.get(key)
+            .then(value => Promise.resolve(JSON.parse(value)));
     }
 
     del(key) {
